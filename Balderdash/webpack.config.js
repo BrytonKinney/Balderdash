3b67d27a10566db863a5055b4576b01094b43047
Bryton Kinney <brytonkinney@gmail.com>
2021-12-22 07:02:52 -0600

    Initial commit

---
+++ Balderdash/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+module.exports = {
+    entry: "./frontend/src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            }
+        ]
+    }
+};
